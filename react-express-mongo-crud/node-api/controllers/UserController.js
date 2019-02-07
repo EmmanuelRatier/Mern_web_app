027c5b6ffe9a5b16f4b85b93a055c3ea41adc256
emmanuel <emmanuel.ratier@epitech.eu>
2019-02-07 14:20:35 +0100

    Create UserController, import model User and create function with query create new user

---
+++ react-express-mongo-crud/node-api/controllers/UserController.js
@@ -0,0 +1,21 @@
+const User = require("../models/User");
+
+exports.listAllUsers = (req, res) => {
+  User.find({}, (err, user) => {
+    console.log(user)
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).json(user);
+  });
+};
+
+exports.createNewUser = (req, res) => {
+  let newUser = new User(req.body.data);
+   newUser.save((err, user) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(201).json(user);
+  }); 
+};
