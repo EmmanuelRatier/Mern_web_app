c9ffe16fc96276460b5c14301a767a45175ca3ad
emmanuel <emmanuel.ratier@epitech.eu>
2019-02-07 14:03:42 +0100

    create node-api folder and add dependencies Express, Body-parser, Mongoose

---
+++ react-express-mongo-crud/node-api/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+//const userController = require("./controllers/UserController");
+const bodyParser = require("body-parser");
+
+// db instance connection
+require("./config/db");
+
+const app = express();
+
+const port = process.env.PORT || 9292;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/* app
+  .route("/users")
+  .get(userController.listAllUsers)
+  .post(userController.createNewUser); */
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
