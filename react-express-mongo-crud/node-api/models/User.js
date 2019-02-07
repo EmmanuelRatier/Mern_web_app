f63d6d4acbce15ff8959054ae602ceb09bdd99db
emmanuel <emmanuel.ratier@epitech.eu>
2019-02-07 14:22:59 +0100

    create folder model and create user model with Schema

---
+++ react-express-mongo-crud/node-api/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Users", UserSchema);
